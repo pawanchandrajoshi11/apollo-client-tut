bab04a938269787d01482264d15f33f16069bfff
Pawan Chandra Joshi <pawanpcj600@gmail.com>
2024-05-27 12:54:21 +0530

    Final Commit - finished query

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useQuery, gql } from "@apollo/client";
 
-function App() {
-  const [count, setCount] = useState(0)
+const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const GET_DOGS = gql`
+  query GetDogs {
+    dogs {
+      id
+      breed
+    }
+  }
+`;
+
+// const GET_DOG_PHOTO = gql`
+// query Dog(${breed: String!}){
+//   dog(breed: $breed){
+//     id
+//     displayImage
+//   }
+// }`
+
+// function DogPhoto({breed}){
+//   const {loading, error, data} = useQuery(GET_DOG_PHOTO, {
+//     variables: {breed}
+//   });
+
+//   if(loading) return null
+//   if(error) return `Error! ${error}`
+
+//   return (
+//     <img src={data.dog.displayImage} style={{ height: 100, width: 100 }} />
+//   );
+
+// }
+
+// function Dogs({ onDogSelected }) {
+//   const { loading, error, data } = useQuery(GET_DOGS);
+//   if (loading) return "Loading...";
+//   if (error) return `Error: ${error.message}`;
+
+//   return (
+//     <select name="dog" onChange={onDogSelected}>
+//       {data.dogs.map((dog) => {
+//         return (
+//           <option key={dog.id} value="dog.breed">
+//             {dog.breed}
+//           </option>
+//         );
+//       })}
+//     </select>
+//   );
+// }
+
+function DisplayLocations() {
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  console.log(data);
+
+  return data.locations.map(({ id, name, description, photo }) => (
+    <div key={id}>
+      <h3>{name}</h3>
+      <img src={`${photo}`} alt="location-reference" width={400} height={250} />
+      <br />
+      <b>About this location</b>
+      <p>{description}</p>
+      <br />
+    </div>
+  ));
 }
 
-export default App
+function App() {
+  return (
+    <div>
+      <h2>My first apollo app</h2>
+      <br />
+      <DisplayLocations />
+    </div>
+  );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -14,8 +14,26 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+// query with a plain text
+// client
+//   .query({
+//     query: gql`
+//       query GetLocations {
+//         locations {
+//           id
+//           name
+//           description
+//           photo
+//         }
+//       }
+//     `,
+//   })
+//   .then((result) => console.log(result));
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
